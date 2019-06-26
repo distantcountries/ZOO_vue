65f2be005265dc1e40ea15b77f8a56763dae2f53
Dragana Trajkovic <distant.countries@gmail.com>
2019-06-26 14:18:16 +0200

    Commit 2

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,29 @@
+<template>
+  
+  <ul>
+    <li v-for="(animal, index) in animals" :key="index">
+        <span style="font-weight:bold;">{{ animal.type }}:</span> {{ animal.name }} {{ animal.date }}
+    </li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+      animals: [
+        { type: 'Parrot', name: 'Pera', date: '17.02.2019' },
+        { type: 'Dog', name: 'Boba', date: '11.07.2013' },
+        { type: 'Cat', name: 'Kitty', date: '' },
+        { type: 'Horse', name: 'Kan', date: '' },
+        { type: 'Sheep', name: 'Shone', date: '21.11.2016' }
+      ],
+
+    } //...return
+  } //...data
+  
+}
+</script>
+
